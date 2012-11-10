961aa3528f0207d97307d1c4caec2b703762fded
Michiel van Oosterhout <michielvoo@gmail.com>
2012-11-10 18:20:06 +0100

    Started new project.

---
+++ Source/Rfc822/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("RFC 822 DateTime")]
+[assembly: AssemblyProduct("RFC 822 DateTime")]
+[assembly: AssemblyDescription("Parse or write a date and time formatted according to the RFC 822 specification.")]
+[assembly: AssemblyCopyright("Copyright 2012 Michiel van Oosterhout")]
+[assembly: AssemblyVersion("0.0.0.0")]
